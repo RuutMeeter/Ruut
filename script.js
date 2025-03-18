99115344678c2364b05d8b2deb9df12ff232aa72
RuutMeeter <musicgamergt@gmail.com>
2025-03-18 13:05:53 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("muudaVarvi").addEventListener("click", function() {
+    document.body.style.backgroundColor = 
+        "#" + Math.floor(Math.random()*16777215).toString(16);
+});
